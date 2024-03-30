85cb1dffbeadb01bbe4ba2c718048351feda11ec
TrevonWright <156366467+TrevonWright@users.noreply.github.com>
2024-03-30 17:31:17 +0000

    V0.2

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 class CarFinder:
   def __init__(self):
-    self.allowed_vehicle_list = ['Ford F-150', 'Chevrolet Silverado',
-     'Tesla CyberTruck', 'Toyota Tundra',
-     'Nissan Titan']
+      self.allowed_vehicle_list = ['Ford F-150', 'Chevrolet Silverado',
+    'Tesla CyberTruck', 'Toyota Tundra',
+    'Nissan Titan']
 
   def print_allowed_vehicles(self):
-    print("The AutoCountry sales manager has allowed the following vehicles "
-      "to be sold:")
-    for vehicle in self.allowed_vehicle_list:
+      print("The AutoCountry sales manager has allowed the following vehicles \
+      to be sold:")
+      for vehicle in self.allowed_vehicle_list:
           print(vehicle)
+
+  def search_vehicle(self):
+      search_input = input("Please Enter the full Vehicle Name: ")
+      if search_input in self.allowed_vehicle_list:
+          print(f"{search_input} is an authorized vehicle")
+      else:
+          print(f"{search_input} is not an authorized vehicle, if you received this in \
+          error please check the spelling and try again")
 
   def display_menu(self):
       print("Welcome to the AutoCountry Car Finder!")
       print("Please select an option:")
       print("1. View allowed vehicles")
-      print("2. Exit")
+      print("2. Search for Authorized Vehicle")
+      print("3. Exit")
 
   def run(self):
       while True:
@@ -24,6 +33,8 @@ class CarFinder:
           if choice == '1':
               self.print_allowed_vehicles()
           elif choice == '2':
+              self.search_vehicle()
+          elif choice == '3':
               print("Thank you for using the AutoCountry Car Finder. Goodbye!")
               break
           else:
@@ -32,3 +43,4 @@ class CarFinder:
 if __name__ == "__main__":
   finder = CarFinder()
   finder.run()
+
