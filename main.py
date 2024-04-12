5c414563b3cbe80227472291c2e62bb79557a5d2
TrevonWright <156366467+TrevonWright@users.noreply.github.com>
2024-04-12 16:50:06 +0000

    V0.3

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
 class CarFinder:
-  def __init__(self):
-      self.allowed_vehicle_list = ['Ford F-150', 'Chevrolet Silverado',
-    'Tesla CyberTruck', 'Toyota Tundra',
-    'Nissan Titan']
+    def __init__(self):
+        self.allowed_vehicle_list = ['Ford F-150', 'Chevrolet Silverado', 'Tesla \
+        CyberTruck', 'Toyota Tundra', 'Nissan Titan']
 
-  def print_allowed_vehicles(self):
-      print("The AutoCountry sales manager has allowed the following vehicles \
-      to be sold:")
-      for vehicle in self.allowed_vehicle_list:
-          print(vehicle)
+    def print_allowed_vehicles(self):
+        print("The AutoCountry sales manager has allowed the following vehicles to be \
+        sold:")
+        for vehicle in self.allowed_vehicle_list:
+            print(vehicle)
 
-  def search_vehicle(self):
-      search_input = input("Please Enter the full Vehicle Name: ")
-      if search_input in self.allowed_vehicle_list:
-          print(f"{search_input} is an authorized vehicle")
-      else:
-          print(f"{search_input} is not an authorized vehicle, if you received this in \
-          error please check the spelling and try again")
+    def search_vehicle(self):
+        search_input = input("Please Enter the full Vehicle Name: ")
+        if search_input in self.allowed_vehicle_list:
+            print(f"{search_input} is an authorized vehicle")
+        else:
+            print(f"{search_input} is not an authorized vehicle, if you received this \
+            in error please check the spelling and try again")
 
-  def display_menu(self):
-      print("Welcome to the AutoCountry Car Finder!")
-      print("Please select an option:")
-      print("1. View allowed vehicles")
-      print("2. Search for Authorized Vehicle")
-      print("3. Exit")
+    def add_vehicle(self):
+        new_vehicle = input("Please Enter the full Vehicle name you would like to add:")
+        self.allowed_vehicle_list.append(new_vehicle)
+        print(f'You have added "{new_vehicle}" as an authorized vehicle')
 
-  def run(self):
-      while True:
-          self.display_menu()
-          choice = input("Enter your choice: ")
+    def display_menu(self):
+        print("********************************")
+        print("AutoCountry Vehicle Finder v0.3")
+        print("********************************")
+        print("Please Enter the following number below from the following menu:\n")
+        print("1. PRINT all Authorized Vehicles")
+        print("2. SEARCH for Authorized Vehicle")
+        print("3. ADD Authorized Vehicle")
+        print("4. Exit")
 
-          if choice == '1':
-              self.print_allowed_vehicles()
-          elif choice == '2':
-              self.search_vehicle()
-          elif choice == '3':
-              print("Thank you for using the AutoCountry Car Finder. Goodbye!")
-              break
-          else:
-              print("Invalid choice. Please try again.")
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                self.print_allowed_vehicles()
+            elif choice == '2':
+                self.search_vehicle()
+            elif choice == '3':
+                self.add_vehicle()
+            elif choice == '4':
+                print("Thank you for using the AutoCountry Vehicle Finder. Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
-  finder = CarFinder()
-  finder.run()
+    finder = CarFinder()
+    finder.run()
+
 
